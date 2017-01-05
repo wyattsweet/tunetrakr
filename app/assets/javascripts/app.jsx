a770c0931968c03f402c7196dfbef5a1215dcb6b
wyattsweet <wyattsweet@gmail.com>
2017-01-04 18:35:19 -0800

    updates

--- app/assets/javascripts/app.jsx
+++ app/assets/javascripts/app.jsx
@@ -1,9 +1,32 @@
-var Application = React.createClass({
-  render: function() {
-    return (
-      <p>Hello, World</p>
-    )
-  }
-})
+//var Tunes = [
+//  {
+//    title: "You'd Be So Nice to Come Home To",
+//    composer: "Cole Porter"
+//  },
+//  {
+//    title: "Satin Doll",
+//    composer: "Billy Strayhorn"
+//  }
+//]
+
+function Application() {
+  return (
+    <div className="application">
+      <div className="header">
+        <h1>TuneTrakr</h1>
+      </div>
+      <button className="addTune">Add Tune</button>
+      <div id="tuneList">
+        <p id="tuneHeader">Tunes You Know</p>
+        <div className="tune">
+          <span>Cole Porter – </span><span>You'd Be So Nice to Come Home To</span>
+        </div> 
+        <div className="tune">
+          <span>Billy Strayhorn – </span><span>Satin Doll</span>
+        </div> 
+      </div>
+    </div>
+  );
+}
 
 ReactDOM.render(<Application/>, document.getElementById('container'));
