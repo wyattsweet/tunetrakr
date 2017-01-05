ed8e210792e80046104879ef190bf8d571917624
wyattsweet <wyattsweet@gmail.com>
2017-01-04 21:24:47 -0800

    updates

--- app/assets/javascripts/app.jsx
+++ app/assets/javascripts/app.jsx
@@ -1,32 +1,81 @@
-//var Tunes = [
-//  {
-//    title: "You'd Be So Nice to Come Home To",
-//    composer: "Cole Porter"
-//  },
-//  {
-//    title: "Satin Doll",
-//    composer: "Billy Strayhorn"
-//  }
-//]
+var TUNES = [
+  {
+    id: 1,
+    title: "You'd Be So Nice to Come Home To",
+    composer: "Cole Porter"
+  },
+  {
+    id: 2,
+    title: "Satin Doll",
+    composer: "Billy Strayhorn"
+  }
+]
 
-function Application() {
+function Topbar(props) {
+  return (
+    <div id="topbar">{props.pageTitle}</div>
+  )
+}
+
+Topbar.propTypes = {
+  pageTitle: React.PropTypes.string.isRequired
+}
+
+function Sidebar(props) {
+  return (
+    <div id="sidebar">
+      <h1>{props.sideBarTitle}</h1>
+      <button className="addTune">Add Tune</button>
+    </div>
+  )
+}
+
+Sidebar.propTypes = {
+  sideBarTitle: React.PropTypes.string.isRequired
+}
+
+function Tune(props) {
+  return (
+    <div className="tune">
+      <span>{props.title}</span>
+    </div> 
+  )
+}
+
+Tune.propTypes = {
+  title: React.PropTypes.string.isRequired
+}
+
+function Application(props) {
   return (
     <div className="application">
-      <div className="header">
-        <h1>TuneTrakr</h1>
-      </div>
-      <button className="addTune">Add Tune</button>
+      <Sidebar sideBarTitle={props.sideBarTitle}/>
+
+      <Topbar pageTitle={props.pageTitle} />
+
       <div id="tuneList">
-        <p id="tuneHeader">Tunes You Know</p>
-        <div className="tune">
-          <span>Cole Porter – </span><span>You'd Be So Nice to Come Home To</span>
-        </div> 
-        <div className="tune">
-          <span>Billy Strayhorn – </span><span>Satin Doll</span>
-        </div> 
+        {props.tunes.map(function(tune) {
+            return <Tune title={tune.title} key={tune.id} />  
+        })}
       </div>
+
     </div>
   );
 }
 
-ReactDOM.render(<Application/>, document.getElementById('container'));
+Application.propTypes = {
+  pageTitle: React.PropTypes.string,
+  sideBarTitle: React.PropTypes.string,
+  tunes: React.PropTypes.arrayOf(React.PropTypes.shape({
+    title: React.PropTypes.string.isRequired,
+    composer: React.PropTypes.string.isRequired,
+    id: React.PropTypes.number.isRequired
+  })).isRequired
+}
+
+Application.defaultProps = {
+  pageTitle: "Repertoire",
+  sideBarTitle: "TT",
+}
+
+ReactDOM.render(<Application tunes={TUNES}/>, document.getElementById('container'));
