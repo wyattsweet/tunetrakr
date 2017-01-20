dbebf9574236dff80521e9f81a72f9b82a173ba5
wyattsweet <wyattsweet@gmail.com>
2017-01-19 23:04:42 -0800

    data model in place

--- server/models.js
+++ server/models.js
@@ -5,4 +5,13 @@ var pg               = require('pg'),
     client           = new pg.Client(connectionString);
 
 client.connect();
-var query = client.query("CREATE TABLE tunes(id init NOT NULL, artist VARCHAR(40) NOT NULL, title VARCHAR(40) NOT NULL)")
+var query = client.query("CREATE TABLE tunes(id SERIAL PRIMARY KEY, \
+                                             created_at timestamp default current_timestamp, \
+                                             updated_at timestamp default current_timestamp,\
+                                             artist VARCHAR(40) NOT NULL, \
+                                             title VARCHAR(40) NOT NULL, \
+                                             key VARCHAR(3), \
+                                             tonality VARCHAR(6), \
+                                             style VARCHAR(20) \
+                                             )");
+query.on('end', () => { client.end(); });
