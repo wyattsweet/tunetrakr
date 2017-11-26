844dfe77c74ee7e1c7a10947df2bd857deb0b5de
wyattsweet <wyattsweet@gmail.com>
2017-11-25 17:26:49 -0800

    header styles

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,20 +1,14 @@
 // Vendor
 import React from 'react';
-import glamorous from 'glamorous';
 
 // Styles
 import styles from './styles.css';
 
-const HeaderWrapper = glamorous.div({
-  background: 'black',
-  height: '60px'
-});
-
 const Header = () => {
   return (
-    <HeaderWrapper>
+    <header className={styles.wrapper}>
       <span className={styles.text}>tuneTrakr</span>
-    </HeaderWrapper>
+    </header>
   );
 };
 
